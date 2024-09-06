5fb59716c3f8a1d871e11885a6a6f641a9977c2a
Mackinlay Kevin <kemack83@gmail.com>
2024-09-06 17:31:59 -0300

    configuracion Tailwind

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,12 @@ import Navbar from './components/Navbar'
 
 function App() {
   return (
-    <div><Navbar /></div>
+    <div>
+      <Navbar />
+    <h1 className='text-3xl font-poppins text-white text-center'>Hello</h1>
+    
+    
+    </div>
   )
 }
 
